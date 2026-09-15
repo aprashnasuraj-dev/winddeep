e11c53fb5d36354a74cad66a278e4cadd263f869
Suraj Dahal <aprashna.suraj@gmail.com>
2026-09-15 06:16:58 +0545

    feat(browser): initialize browser automation package

---
+++ app/browser/__init__.py
@@ -0,0 +1,5 @@
+"""Browser automation primitives for Windeep."""
+
+from app.browser.automation import BrowserAutomationEngine, BrowserAutomationError, BrowserResult
+
+__all__ = ["BrowserAutomationEngine", "BrowserAutomationError", "BrowserResult"]
